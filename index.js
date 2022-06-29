8b9b845a534a8f0dfb129243c04cb379828823e6
Hunter Birch <hunter.macka.birch@gmail.com>
2022-06-29 14:13:19 -0600

    hopeful this will work

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const path = require('path')
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
 const port = process.env.PORT || 4004
 
 app.listen(port, () => {
